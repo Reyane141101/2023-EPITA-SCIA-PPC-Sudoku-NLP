a95c58756dcd2fc5b64a3bb0fffddd7378580ab1
Rostan <rostan.tabet@gmail.com>
2023-03-09 17:10:46 +0100

    OrToolsSolver: start initializing a simple model

--- Sudoku.ORTools/OrToolsSolver.cs
+++ Sudoku.ORTools/OrToolsSolver.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Google.OrTools.Sat;
 using Sudoku.Shared;
 
 namespace Sudoku.OrTools
@@ -12,6 +8,27 @@ namespace Sudoku.OrTools
 		public SudokuGrid Solve(SudokuGrid s)
 		{
 			return s;
+		}
+
+		/**
+		 * Initialize a model with n*n from a sudoku grid
+		 */
+		private CpModel InitModel(SudokuGrid s)
+		{
+			var model = new CpModel();
+			
+			const int size = 9;
+			var cellVars = new IntVar[size, size];
+			for (var i = 0; i < size; i++)
+			{
+				for (var j = 0; j < size; j++)
+				{
+					// TODO: Initialize the known values
+					cellVars[i, j] = model.NewIntVar(1, size, $"x{i}{j}");
+				}
+			}
+			
+			return model;
 		}
 	}
 }
