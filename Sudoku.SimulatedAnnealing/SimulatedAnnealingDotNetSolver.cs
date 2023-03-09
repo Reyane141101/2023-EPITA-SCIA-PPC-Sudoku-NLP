78f19df6ab437dbd7bf33ee88b8b306744995a1c
quentin.fisch <quentin.fisch@epita.fr>
2023-03-09 16:19:36 +0100

    feat: create empty simulated annealing solver

---
+++ Sudoku.SimulatedAnnealing/SimulatedAnnealingDotNetSolver.cs
@@ -0,0 +1,12 @@
+using Sudoku.Shared;
+
+namespace Sudoku.SimulatedAnnealing
+{
+    public class SimulatedAnnealingDotNetSolver: ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s;
+        }
+    }    
+}
