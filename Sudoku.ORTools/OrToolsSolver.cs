3577aa7a0ecaca447730e1c26d7797d4c88191bc
Celian RAIMBAULT <celian.raimbault@epita.fr>
2023-03-09 15:50:10 +0100

    ortools: Added empty project

---
+++ Sudoku.ORTools/OrToolsSolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sudoku.Shared;
+
+namespace Sudoku.OrTools
+{
+	public class OrToolsSolver : ISudokuSolver
+	{
+		public SudokuGrid Solve(SudokuGrid s)
+		{
+			return s;
+		}
+	}
+}
