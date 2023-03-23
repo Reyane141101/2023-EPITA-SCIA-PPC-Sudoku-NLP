a5f3c9e2a473bce50a00dbfb2c4ca4500fcdb463
Gilles Recouvreux <gilles.recouvreux@epita.fr>
2023-03-23 14:53:37 +0100

    feat(DlxLib): project start our solver, actually only empty solver

---
+++ Sudoku.DlxLib/DlxLibEmptySolver.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.DlxLib;
+
+public class DlxLibEmptySolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        Console.WriteLine(@"===== DlixLib Solver =====");
+        return s.CloneSudoku();
+    }
+}
