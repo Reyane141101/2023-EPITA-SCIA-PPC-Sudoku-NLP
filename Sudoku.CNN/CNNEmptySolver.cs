f43ff08280bb4d34419300116b0839d9c04dd89d
Gilles Recouvreux <gilles.recouvreux@epita.fr>
2023-03-29 18:26:35 +0200

    fix(rename): rename folder and files

---
+++ Sudoku.CNN/CNNEmptySolver.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.DlxLib;
+
+public class DlxLibEmptySolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        Console.WriteLine(@"===== DlixLib Empty Solver =====");
+        return s.CloneSudoku();
+    }
+}
