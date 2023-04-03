671b665e8ab97b48daa4813944bc21d938e52918
Your Name <you@example.com>
2023-04-03 09:19:30 +0200

    base version of the graph coloring solution to the sudoku problem

---
+++ Sudoku.GraphColoration/GraphColorationSolver.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sudoku.Shared;
+using QuickGraph;
+using GraphSharp;
+using GraphSharp.Algorithms.Layout.Contextual;
+
+
+namespace Graph_Coloration_Solver
+{
+	public class GraphColorationSolver : ISudokuSolver
+	{
+
+		public SudokuGrid Solve(SudokuGrid s)
+		{
+			var adjacencyMatrix = BuildAdjencyMatrix();
+			var solver = new VertexColoringSolver();
+			var vertexColors = SudokuToVertexColor(s);
+			
+			//launch the solver
+			var colors = solver.Solve(adjacencyMatrix, s.Cells.Length, vertexColors);
+
+			if (colors != null)
+				foreach (KeyValuePair<int, int> color in colors)
+					s.Cells[color.Key / 9][color.Key % 9] = color.Value;
+			else
+				Console.WriteLine("No valid coloring exists.");
+			
+			return s;
+		}
+
+		private Dictionary<int, int> SudokuToVertexColor(SudokuGrid sudoku)
+		{
+			var vertexColors = new Dictionary<int, int>();
+			for (int row = 0; row < sudoku.Cells.Length; row++)
+			{
+				for (int col = 0; col < sudoku.Cells.Length; col++)
+					if (sudoku.Cells[row][col] != 0)
+						vertexColors[row + col * sudoku.Cells.Length] = sudoku.Cells[row][col];
+			}
+
+			return vertexColors;
+		}
+
+		private int[][] BuildAdjencyMatrix()
+		{
+			int[][] adjacencyMatrix = new int[81][];
+
+			for (int i = 0; i < 81; i++)
+			{
+				adjacencyMatrix[i] = new int[81];
+				for (int j = 0; j <= i; j++)
+				{
+					if (j / 9 == i / 9 || j % 9 == i % 9 || (j / 27 == i / 27 && j % 9 / 3 == i % 9 / 3))
+					{
+						adjacencyMatrix[i][j] = 1;
+						adjacencyMatrix[j][i] = 1;
+					}
+					else
+						adjacencyMatrix[i][j] = 0;
+				}
+			}
+
+			return adjacencyMatrix;
+		}
+		
+	}
+}
+
---
+++ Sudoku.GraphColoration/VertexColoringSolver.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Graph_Coloration_Solver
+{
+    public class VertexColoringSolver
+    {
+        public Dictionary<int, int> Solve(int[][] adjacencyMatrix, int numColors, Dictionary<int, int> vertexColors)
+        {
+            var availableColors = Enumerable.Range(1, numColors).ToList();
+
+            for (int vertex = 0; vertex < adjacencyMatrix.Length; vertex++)
+            {
+                // Find the colors of the adjacent vertices
+                var adjacentColors = new HashSet<int>();
+                for (int neighbor = 0; neighbor < adjacencyMatrix[vertex].Length; neighbor++)
+                {
+                    if (adjacencyMatrix[vertex][neighbor] == 1 && vertexColors.ContainsKey(neighbor))
+                    {
+                        adjacentColors.Add(vertexColors[neighbor]);
+                    }
+                }
+
+                // Find the first available color for the vertex
+                var color = availableColors.FirstOrDefault(c => !adjacentColors.Contains(c));
+                if (color != 0)
+                {
+                    vertexColors[vertex] = color;
+                }
+                else
+                {
+                    // Backtrack if no color is available
+                    vertexColors.Remove(vertex);
+                    vertex--;
+                    if (vertex < 0)
+                    {
+                        // No valid coloring exists
+                        return null;
+                    }
+                }
+            }
+
+            return vertexColors;
+        }
+    }
+}
