107119711d2eb8498b575864880a84adecf3d53d
Céline Huang <celine.huang@epita.fr>
2023-04-08 19:40:24 +0200

    clean code

--- Sudoku.ChocoSolver/ChocoSolver.cs
+++ Sudoku.ChocoSolver/ChocoSolver.cs
@@ -5,23 +5,8 @@ namespace Sudoku.ChocoSolver;
 
 public class ChocoSolver : ISudokuSolver
 {
-	//private string GridToString(SudokuGrid s)
-	//{
-	// string GridString = "";
-	//    int[][] cells = s.Cells;
-	//    for (int i = 0; i < cells.Length; i++)
-	//    {
-	//        for (int j = 0; j < cells[i].Length; j++)
-	//        {
-	//            GridString += cells[i][j];
-	//        }
-	//    }
-	//    return GridString;
-	//}
 	public SudokuGrid Solve(SudokuGrid s)
 	{
-		// Appeler la fonction codée en Java
-
 		var toSolve = s.CloneSudoku();
 
 		var javaSolver = new sudoku.chocosolver.SudokuCP(toSolve.Cells);
--- Sudoku.ChocoSolver/ChocoSolverJava/src/main/java/sudoku/chocosolver/SudokuCP.java
+++ Sudoku.ChocoSolver/ChocoSolverJava/src/main/java/sudoku/chocosolver/SudokuCP.java
@@ -8,61 +8,56 @@ import org.chocosolver.solver.variables.IntVar;
 
 
 public class SudokuCP extends Object {
-    public static void main(String[] args) {
-
-    }
-
-    public static void PrintCells(int[][] cells) {
-        String cellsString = "";
-        for (int i = 0; i < cells.length; i++) {
-            for (int j = 0; j < cells[i].length; j++) {
-                cellsString += cells[i][j];
-            }
-        }
-        System.out.println(cellsString);
-    }
 
     private Model model;
     private int[][] sudoku;
     private int[] validEntries = {1,2,3,4,5,6,7,8,9};
     private int side ;
-    private IntVar[][] vs;
+    private IntVar[][] grid;
 
     public SudokuCP(int[][] sudoku) {
         this.sudoku = sudoku;
+
+        // New model
         model = new Model();
 
+        // Dimensions
         final int dimensionality = 3;
         side = dimensionality * dimensionality;
-        vs = model.intVarMatrix(side, side, 1, validEntries.length);
 
+        // Initialize the grid
+        grid = model.intVarMatrix(side, side, 1, validEntries.length);
+
+        // Fill cells from initialized grid
         for (int i = 0; i < side; i++) {
             for (int j = 0; j < side; j++) {
                 int c = sudoku[i][j];
                 if (c != 0) {
-                    model.arithm(vs[i][j], "=", c).post();
+                    model.arithm(grid[i][j], "=", c).post();
                 }
             }
         }
 
+        // Constraint on rows and columns
         for (int i = 0; i < side; i++) {
             IntVar[] row = new IntVar[side];
             IntVar[] column = new IntVar[side];
             for (int j = 0; j < side; j++) {
-                row[j] = vs[i][j];
-                column[j] = vs[j][i];
+                row[j] = grid[i][j];
+                column[j] = grid[j][i];
             }
             model.allDifferent(row).post();
             model.allDifferent(column).post();
         }
 
+        // Constraint on sections
         for (int i = 0; i < dimensionality; i++) {
             for (int j = 0; j < dimensionality; j++) {
                 IntVar[] section = new IntVar[side];
                 int idx = 0;
                 for (int xoff = 0; xoff < dimensionality; xoff++) {
                     for (int yoff = 0; yoff < dimensionality; yoff++) {
-                        section[idx++] = vs[i * dimensionality + xoff][j * dimensionality + yoff];
+                        section[idx++] = grid[i * dimensionality + xoff][j * dimensionality + yoff];
                     }
                 }
                 model.allDifferent(section).post();
@@ -71,20 +66,23 @@ public class SudokuCP extends Object {
     }
 
     public boolean solve() {
+        // Call solve method and return true if a solution is found, false otherwise
         boolean solved = model.getSolver().solve();
         if (!solved) {
             return false;
         }
 
+        // Get the solution
         ArrayList<ArrayList<Integer>> sol = new ArrayList<ArrayList<Integer>>(side);
         for (int i = 0; i < side; i++) {
             ArrayList<Integer> row = new ArrayList<Integer>();
             for (int j = 0; j < side; j++) {
-                row.add(vs[i][j].getValue() - 1);
+                row.add(grid[i][j].getValue() - 1);
             }
             sol.add(row);
         }
 
+        // Search for more solutions
         boolean moreSolutions = model.getSolver().solve();
         if (moreSolutions) {
             return false;
