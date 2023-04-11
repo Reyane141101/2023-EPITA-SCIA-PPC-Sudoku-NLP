462c8befeb6ff3ea5a792e3924c72f60edb06185
Gabriel Huet de Froberville <gabriel.huet-de-froberville@epita.fr>
2023-04-11 15:42:10 +0200

    fix(ORtools: solver): Fix solver and format code

--- Sudoku.ORTools/OrToolsSolver.cs
+++ Sudoku.ORTools/OrToolsSolver.cs
@@ -1,77 +1,73 @@
 using Google.OrTools.Sat;
 using Sudoku.Shared;
 
-namespace Sudoku.OrTools
+namespace Sudoku.OrTools;
+
+public class OrToolsSolver : ISudokuSolver
 {
-    public class OrToolsSolver : ISudokuSolver
+    private const int Size = 9;
+
+    public SudokuGrid Solve(SudokuGrid s)
     {
-        private const int Size = 9;
+        CpSolver solver = new();
+        (CpModel model, IntVar[,] vars) = InitModel(s);
+        solver.Solve(model);
 
-        public SudokuGrid Solve(SudokuGrid s)
-        {
-            var solver = new CpSolver();
-            var (model, vars) = InitModel(s);
-            solver.Solve(model);
+        for (int i = 0; i < Size; i++)
+        for (int j = 0; j < Size; j++)
+            s.Cells[i][j] = (int)solver.Value(vars[i, j]);
 
-            for (var i = 0; i < Size; i++)
-            for (var j = 0; j < Size; j++)
-                s.Cells[i][j] = (int)solver.Value(vars[i, j]);
+        return s;
+    }
 
-            return s;
-        }
-
-        /**
+    /**
 		 * Initialize a model with n*n from a sudoku grid
 		 */
-        private static (CpModel, IntVar[,]) InitModel(SudokuGrid grid)
-        {
-            var model = new CpModel();
+    private static (CpModel, IntVar[,]) InitModel(SudokuGrid grid)
+    {
+        CpModel model = new();
 
-            var cellVars = new IntVar[Size, Size];
-            for (var i = 0; i < Size; i++)
+        IntVar[,] cellVars = new IntVar[Size, Size];
+        for (int i = 0; i < Size; i++)
+        for (int j = 0; j < Size; j++)
+            if (grid.Cells[i][j] != 0)
+                cellVars[i, j] = model.NewConstant(grid.Cells[i][j]);
+            else
+                cellVars[i, j] = model.NewIntVar(1, Size, $"x{i}{j}");
+
+        AddConstraints(model, cellVars);
+
+        return (model, cellVars);
+    }
+
+    private static void AddConstraints(CpModel model, IntVar[,] vars)
+    {
+        for (int i = 0; i < Size; i++)
+        {
+            IntVar[] row = new IntVar[Size];
+            IntVar[] col = new IntVar[Size];
+            for (int j = 0; j < Size; j++)
             {
-                for (var j = 0; j < Size; j++)
-                {
-                    if (grid.Cells[i][j] != 0)
-                        cellVars[i, j] = model.NewConstant(grid.Cells[i][j]);
-                    else
-                        cellVars[i, j] = model.NewIntVar(1, Size, $"x{i}{j}");
-                }
+                row[j] = vars[i, j];
+                col[j] = vars[j, i];
             }
 
-            AddConstraints(model, cellVars);
-
-            return (model, cellVars);
+            model.AddAllDifferent(row);
+            model.AddAllDifferent(col);
         }
 
-        private static void AddConstraints(CpModel model, IntVar[,] vars)
-        {
-            for (var i = 0; i < Size; i++)
-            {
-                var row = new IntVar[Size];
-                var col = new IntVar[Size];
-                for (var j = 0; j < Size; j++)
-                {
-                    row[j] = vars[i, j];
-                    col[j] = vars[j, i];
+        for (int i = 0; i < Size; i++)
+        for (int j = 0; j < Size; j++)
+            if (i % 3 == 0 && j % 3 == 0)
+                model.AddAllDifferent(AddBoxConstraints(vars, i, j));
+    }
 
-                    if (i % 3 == 0 && j % 3 == 0)
-                        model.AddAllDifferent(AddBoxConstraints(vars, i, j));
-                }
-
-                model.AddAllDifferent(row);
-                model.AddAllDifferent(col);
-            }
-        }
-
-        private static IEnumerable<IntVar> AddBoxConstraints(IntVar[,] vars, int startX, int startY)
-        {
-            var box = new IntVar[9];
-            for (var i = startX; i < startX + 3; i++)
-            for (var j = startY; j < startY + 3; j++)
-                box[i * 3 * j] = vars[i, j];
-
-            return box;
-        }
+    private static IEnumerable<IntVar> AddBoxConstraints(IntVar[,] vars, int startX, int startY)
+    {
+        IntVar[] box = new IntVar[9];
+        for (int i = startX; i < startX + 3; i++)
+        for (int j = startY; j < startY + 3; j++)
+            box[(i - startX) * 3 + (j - startY)] = vars[i, j];
+        return box;
     }
 }
