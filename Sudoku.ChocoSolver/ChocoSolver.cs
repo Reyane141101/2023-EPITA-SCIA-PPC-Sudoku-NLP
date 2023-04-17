8fc165887da947300e5908d3b79fee53df15a364
Jean-Sylvain Boige <jsboige@gmail.com>
2023-04-17 10:39:02 +0200

    Déclinaisons solvers choco

--- Sudoku.ChocoSolver/ChocoSolver.cs
+++ Sudoku.ChocoSolver/ChocoSolver.cs
@@ -3,7 +3,7 @@ using sudoku.chocosolver;
 
 namespace Sudoku.ChocoSolver;
 
-public class ChocoSolver : ISudokuSolver
+public abstract class ChocoSolverBase : ISudokuSolver
 {
 	public SudokuGrid Solve(SudokuGrid s)
 	{
@@ -11,28 +11,30 @@ public class ChocoSolver : ISudokuSolver
 		var javaSolver = new sudoku.chocosolver.SudokuCP(toSolve.Cells);
 		
 
-		Console.WriteLine("Choose a strategy :\n1 - Default\n2 - InputOrderLB\n3 - MinDomLB\n4 - ActivityBased\n5 - DomOverWDeg");
-		string strategie = Console.ReadLine();
+		//Console.WriteLine("Choose a strategy :\n1 - Default\n2 - InputOrderLB\n3 - MinDomLB\n4 - ActivityBased\n5 - DomOverWDeg");
+		//string strategie = Console.ReadLine();
+
+		int strategie = GetStrategy();
 		
 		switch (strategie)
 		{
-			case "1":
+			case 1:
 				javaSolver.solveDefault();
 				break;
 			
-			case "2":
+			case 2:
 				javaSolver.solveInputOrderLB();
 				break;
 
-			case "3":
+			case 3:
 				javaSolver.solveMinDomLB();
 				break;
 			
-			case "4":
+			case 4:
 				javaSolver.solveActivityBased();
 				break;
 			
-			case "5":
+			case 5:
 				javaSolver.solveDomOverWDeg();
 				break;
 
@@ -42,5 +44,47 @@ public class ChocoSolver : ISudokuSolver
 		}
 
 		return toSolve;
+	}
+
+	protected abstract int GetStrategy();
+}
+
+public class  ChocoSolverDefault : ChocoSolverBase
+{
+	protected override int GetStrategy()
+	{
+		return 1;
+	}
+}
+
+public class ChocoSolverOrderLB : ChocoSolverBase
+{
+	protected override int GetStrategy()
+	{
+		return 2;
+	}
+}
+
+public class ChocoSolverMinDomLB : ChocoSolverBase
+{
+	protected override int GetStrategy()
+	{
+		return 3;
+	}
+}
+
+public class ChocoSolverActivityBased : ChocoSolverBase
+{
+	protected override int GetStrategy()
+	{
+		return 4;
+	}
+}
+
+public class ChocoSolverDomOverWDeg : ChocoSolverBase
+{
+	protected override int GetStrategy()
+	{
+		return 5;
 	}
 }
