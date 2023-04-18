a6c9577ea47c4e1dfda7653b72b9b5d3ad521e8d
unknown <you@example.com>
2023-04-18 09:46:38 +0200

    Start of README and test of tactic

---
+++ Sudoku.Z3Solver/README.md
@@ -0,0 +1,16 @@
+# Résolveur de Sudoku utilisant la librairie Z3
+
+Adrien GIGET
+Ethan MACHAVOINE
+Jonathan POELGER
+
+
+Vous devrez également inclure un fichier Readme.md dans votre projet expliquant 
+votre approche et présentant les résultats obtenus lors des tests de performance 
+(attention à ne pas modifier le readme du répertoire principal). Vous pouvez 
+utiliser le projet Sudoku.Benchmark pour effectuer ces tests de performance et 
+mesurer le temps d'exécution de votre solver sur différentes grilles de Sudoku.
+
+## Approche
+
+Pour ce projet, nous avons utiliser la librairie Z3 de solveurs 
---
+++ Sudoku.Z3Solver/Z3Tactics.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Z3;
+using Sudoku.Shared;
+
+namespace Sudoku.Z3Solver
+{
+    public class Z3Tactics : Z3BitVectorSolverBase
+    {
+        public override SudokuGrid Solve(SudokuGrid s)
+        {
+            SudokuGrid solution = new SudokuGrid();
+            SudokuSolve(s, ref solution);
+            return solution;
+        }
+
+        public void SudokuSolve(SudokuGrid grid, ref SudokuGrid solution)
+        {
+            Solver solver = ReusableSolver;
+            solver.Push();
+
+            BoolExpr puzzleConstraints = GetPuzzleConstraints(grid);
+            solver.Assert(puzzleConstraints);
+
+            Tactic tactic = ctx.MkTactic("smt");
+            Goal goal = ctx.MkGoal();
+            goal.Assert(ctx.MkAnd(GenericContraints, puzzleConstraints));
+            ApplyResult applyResult = tactic.Apply(goal);
+
+            if (applyResult.NumSubgoals > 0)
+            {
+                Goal newGoal = applyResult.Subgoals[0];
+                solver.Assert(newGoal.Formulas);
+
+                if (solver.Check() == Status.SATISFIABLE)
+                {
+                    Model model = solver.Model;
+                    for (int i = 0; i < 9; i++)
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            solution.Cells[i][j] = ((BitVecNum)model.Evaluate(CellVariables[i][j])).Int;
+                        }
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException("Sudoku puzzle could not be solved.");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("No subgoals were produced by the tactic.");
+            }
+
+            solver.Pop();
+        }
+    }
+}
